7b485b38cfaa2a98a481e39e7a98033ad6e42f07
Nick Alt <altnp291@gmail.com>
2020-11-07 00:24:20 -0500

    stuff

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
-import Nav from "./componenets/Nav"
+import Nav from "./componenets/Nav";
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 function App() {
-  return (
-    <div>
-      <Nav />
-      <div>Hello World</div>
-    </div>
-  );
+    return (
+        <Router>
+            <div>
+                <Nav />
+                <Switch>
+                    <Route path="/hello" render={() => <div>hello</div>}/>
+                    <Route path="/world">
+                        <div>World</div>
+                    </Route>
+                </Switch>
+            </div>
+        </Router>
+    );
 }
 
 export default App;
--- src/componenets/Nav.js
+++ src/componenets/Nav.js
@@ -1,8 +1,12 @@
+import { Link } from 'react-router-dom';
+
 function Nav() {
     return (
-    <div>
-        <li>Link</li>
-    </div>)
+        <div>
+            <Link to="hello">Hello</Link>
+            <Link to="world">World</Link>
+        </div>
+    );
 }
 
 export default Nav;
