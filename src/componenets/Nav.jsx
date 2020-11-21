3cbc011c4bb6e6208f8bded3f961ba147e6aea3f
Nick Alt <altnp291@gmail.com>
2020-11-21 13:07:20 -0500

    side nav

--- src/componenets/Nav.jsx
+++ src/componenets/Nav.jsx
@@ -1,16 +1,54 @@
 import { Link } from 'react-router-dom';
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import PropTypes from 'prop-types';
-import routes from '../routes';
+import styles from './nav.module.scss';
 
 function Nav(props) {
+  // eslint-disable-next-line no-unused-vars
   const { helloText, worldText } = props;
 
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
+  const divRef = useRef();
+  const handleClick = (e) => {
+    if (!divRef.current.contains(e.target)) {
+      setMenuOpen(false);
+    }
+  };
+
+  useEffect(() => {
+    if (menuOpen) {
+      document.addEventListener('mousedown', handleClick, false);
+      return () => {
+        document.removeEventListener('mousedown', handleClick);
+      };
+    }
+
+    return () => {};
+  }, [menuOpen]);
+
+  // eslint-disable-next-line react/prop-types
+  const ToggleLink = ({ to, children }) => (<Link to={to} onClick={toggleMenu}>{children}</Link>);
+
   return (
-    <div>
-      <Link to={routes.hello}>{helloText}</Link>
-      <Link to={routes.world}>{worldText}</Link>
-    </div>
+    <>
+      <nav className={styles.topNav}>
+        <button type="button" className={`material-icons ${styles.menuBtn}`} onClick={() => toggleMenu()}>menu</button>
+        <Link to="/hello">
+          <img src="/images/three-houses-logo.png" alt="three houses app" className={styles.navImg} />
+        </Link>
+        <Link to="/about">About</Link>
+
+      </nav>
+      <div className={`${styles.overlay} ${menuOpen ? styles.overlayDisplayed : ''}`} />
+      <div ref={divRef} className={`${styles.sideNav} ${menuOpen ? styles.sideNavOpen : ''}`}>
+        <ToggleLink to="/about">About</ToggleLink>
+      </div>
+    </>
   );
 }
 
